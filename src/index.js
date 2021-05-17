e3a71e1fce9317fd760def947a81b0b72db96e1d
TaLhA ZuBaIr MaYo <tkashi328@gmail.com>
2021-05-17 23:17:32 +0500

    Final

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {BrowserRouter} from 'react-router-dom';
+import Apps from "./Apps";
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Apps />
+  </BrowserRouter>
+
+ 
+   ,
+  document.getElementById('root')
+);
+
